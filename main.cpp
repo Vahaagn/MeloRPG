377915c8a25a4abee34f3f5659f4cedda72b3681
Vahaagn <matgizq@gmail.com>
2016-10-12 23:55:39 +0200

    Added CMake project

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "config.h"
+#include <iostream>
+#include <SFML/Graphics.hpp>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+    //cout << "Version " << MeloRPG_VERSION_MAJOR << "." << MeloRPG_VERSION_MINOR << endl;
+
+    std::string windowTitle("Yo!");
+
+    std::cout<< windowTitle << std::endl;
+
+    char c;
+    std::cin>>c;
+}
